d2cc82f39f0933dd4d23bfc2adf631cd36d80b61
natamartins <nata.codedev@gmai.com>
2023-06-27 19:14:17 -0300

    new upload

--- src/api/Urls.tsx
+++ src/api/Urls.tsx
@@ -3,4 +3,3 @@ export const REACT_KEY = "6e559a55d559f685bdb9982b8b159c72"
 export const REACT_IMG_PATH = "https://image.tmdb.org/t/p/w1280"
 export const REACT_IMG = "https://image.tmdb.org/t/p/w500"
 export const REACT_API_KEY = '&api_key=6e559a55d559f685bdb9982b8b159c72&language=pt-BR'
-// export const REACT_KEY = "6e559a55d559f685bdb9982b8b159c72"
